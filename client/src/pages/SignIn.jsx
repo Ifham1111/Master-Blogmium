4536cd1cd4cc2ee60b2345b93913e3ad5ba97b33
Mohamed MRI <mohamedmri.21@uom.lk>
2024-01-29 23:56:33 +0530

    Add gap between elements in SignIn page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -66,6 +66,8 @@ export default function SignIn() {
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+
+        
         {/* left */}
         <div className='flex-1'>
           <Link
@@ -82,7 +84,7 @@ export default function SignIn() {
           </p>
         </div>
 
-        
+
         {/* right */}
         <div className='flex-1'>
           <form className='flex flex-col gap-4'
