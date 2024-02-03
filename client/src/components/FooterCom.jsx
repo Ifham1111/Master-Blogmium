f82907be4e9073aa14a4c77dd201fdf58965aa78
Mohamed MRI <mohamedmri.21@uom.lk>
2024-02-03 23:50:04 +0530

    Remove empty lines in FooterCom component

--- client/src/components/FooterCom.jsx
+++ client/src/components/FooterCom.jsx
@@ -66,15 +66,14 @@ export default function FooterCom() {
                         </div>
                     </div>
                 </div>
-                
-                
+
+
                 <Footer.Divider />
                 <div className='w-full sm:flex sm:items-center sm:justify-between'>
                     <Footer.Copyright href='#' by="Ifham's Blog" year={new Date().getFullYear()} />
                     <div className='flex gap-6 sm:mt-0 mt-4 sm:justify-center'>
                         <Footer.Icon href='#' icon={BsFacebook} />
                         <Footer.Icon href='#' icon={BsInstagram} />
-                        
                         <Footer.Icon href='#' icon={BsTwitterX} />
                         <Footer.Icon href='#' icon={BsGithub} />
                         <Footer.Icon href='#' icon={BsDiscord} />
